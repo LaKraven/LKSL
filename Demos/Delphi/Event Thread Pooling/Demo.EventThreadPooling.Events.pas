31516d163cc5e7e6960962e6cc8c7c2fc792719c
LaKraven <simon@lakraven.com>
2015-03-05 12:20:46 +0000

    Removed all remnants of LKSL.Events.Base.pas and the unit itself.

--- Demos/Delphi/Event Thread Pooling/Demo.EventThreadPooling.Events.pas
+++ Demos/Delphi/Event Thread Pooling/Demo.EventThreadPooling.Events.pas
@@ -13,40 +13,16 @@ type
   TLKDemoResponseEvent = class;
 
   TLKDemoEvent = class(TLKEvent)
-  protected
-    {$IFNDEF LKSL_EVENTENGINE_REFCOUNT}
-      procedure Clone(const AFromEvent: TLKEvent); override;
-    {$ENDIF LKSL_EVENTENGINE_REFCOUNT}
+
   end;
 
   TLKDemoResponseEvent = class(TLKEvent)
-  protected
-    {$IFNDEF LKSL_EVENTENGINE_REFCOUNT}
-      procedure Clone(const AFromEvent: TLKEvent); override;
-    {$ENDIF LKSL_EVENTENGINE_REFCOUNT}
+
   end;
 
   TLKDemoEventListener = class(TLKEventListener<TLKDemoEvent>);
   TLKDemoResponseEventListener = class(TLKEventListener<TLKDemoResponseEvent>);
 
 implementation
-
-{ TLKDemoEvent }
-
-{$IFNDEF LKSL_EVENTENGINE_REFCOUNT}
-  procedure TLKDemoEvent.Clone(const AFromEvent: TLKEvent);
-  begin
-
-  end;
-{$ENDIF LKSL_EVENTENGINE_REFCOUNT}
-
-{ TLKDemoResponseEvent }
-
-{$IFNDEF LKSL_EVENTENGINE_REFCOUNT}
-  procedure TLKDemoResponseEvent.Clone(const AFromEvent: TLKEvent);
-  begin
-
-  end;
-{$ENDIF LKSL_EVENTENGINE_REFCOUNT}
 
 end.
